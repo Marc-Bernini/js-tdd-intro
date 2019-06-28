769a2818568c6814e552247997f080eb65119d55
Marc Bernini <bernini.marc@gmail.com>
2019-06-28 09:40:41 +0200

    Tests written

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,15 @@
+// capitalizeFirstLetters.js
+const assert = require('assert');
+
+// WRITE THE ACTUAL FUNCTION HERE
+
+// Check that capitalizeFirstLetters is a function
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+// Check that capitalizeFirst accepts one argument
+assert.strictEqual(capitalizeFirstLetters.length, 1);
+// Check that capitalizeFirst transforms javaScript correctly
+assert.strictEqual(capitalizeFirstLetters('javaScript est un langage informatique'), 'JavaScript Est Un Langage Informatique');
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirstLetters(''), '');
